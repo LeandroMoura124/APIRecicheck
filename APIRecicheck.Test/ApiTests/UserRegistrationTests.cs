ebec3c4327ce0bc8d4744085976b7f18ce1ae153
LeandroMoura124 <leofcb334@gmail.com>
2024-11-02 21:43:51 -0300

    Inserção - Test Runner UserRegistration

---
+++ APIRecicheck.Test/ApiTests/UserRegistrationTests.cs
@@ -0,0 +1,42 @@
+﻿using Xunit;
+using System.Net.Http;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+public class UserRegistrationTests : IClassFixture<WebApplicationFactory<APIRecicheck.Program>>
+{
+    private readonly HttpClient _client;
+
+    public UserRegistrationTests(WebApplicationFactory<APIRecicheck.Program> factory)
+    {
+        _client = factory.CreateClient();
+    }
+
+    [Fact]
+    public async Task Login_ShouldReturnToken_WhenValidCredentials()
+    {
+        // Arrange
+        var user = new 
+        { 
+            UserId = 1, 
+            Username = "Leandro", 
+            Password = "123456", 
+            Role = "gerente" 
+        };
+        var content = new StringContent(JsonConvert.SerializeObject(user), System.Text.Encoding.UTF8, "application/json");
+
+        // Act
+        var response = await _client.PostAsync("/api/auth/login", content);
+        var responseBody = await response.Content.ReadAsStringAsync();
+
+        // Assert Status Code
+        response.StatusCode.Should().Be(System.Net.HttpStatusCode.OK);
+
+        // Assert Response Body
+        JObject json = JObject.Parse(responseBody);
+        json["token"].Value<string>().Should().NotBeNullOrEmpty();
+    }
+}
